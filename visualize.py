be8601e16e5796232df50425311bf94071c050d1
quockhanhcao <57025493+quockhanhcao@users.noreply.github.com>
2021-01-01 13:46:06 +0700

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -7,11 +7,10 @@ country = json.load(open("countries.geojson", "r"))
 df = pd.read_csv('forward_country.csv')
 country_code_map = {}
 
+#map the code in geojson file of each country in csv file
 for feature in country['features']:
     feature['code'] = feature['properties']['ISO_A3']
     country_code_map[feature['properties']['ADMIN']] = feature['code']
-
-
 df['code'] = df['Country'].apply(lambda x: country_code_map[x])
 
 
